faf257338cd41d5c0bde096f85f5e49af91c9449
Maroš Hajník <hajnik.work@gmail.com>
2021-04-28 15:38:16 +0200

    In memory user details manager

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -4,10 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -18,5 +23,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails annasmithUser = User.builder()
+                .username("annasmith")
+                .password("password")
+                .roles("STUDENT") // this will be ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                annasmithUser
+        );
     }
 }
